6f24661d82dfc02bf5cff0ce9919b76abb3c4252
guillemnobi <guillemguillem01@gmail.com>
2021-09-14 09:37:11 +0200

    changes 1409 m

---
+++ index.js
@@ -0,0 +1,52 @@
+
+console.log("js is working today")
+
+
+$.ajax({
+  type: 'POST',
+  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=136',
+  contentType: 'application/json',
+  dataType: 'json',
+  data: JSON.stringify({
+    task: {
+      content: 'Do something fun'
+    }
+  }),
+  success: function (response, textStatus) {
+    console.log(response);
+  },
+  error: function (request, textStatus, errorMessage) {
+    console.log(errorMessage);
+  }
+});
+
+
+// $(document).ready(function(){
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+//     dataType: 'json',
+//     success: function (response, textStatus) {
+//         console.log(response);
+//     },
+//     error: function (request, textStatus, errorMessage) {
+//       console.log(errorMessage);
+//     }
+//   });
+// });
+
+// $(document).ready(function(){
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=136',
+//     dataType: 'json',
+//     success: function (response, textStatus) {
+//       response.tasks.forEach(function (task) {
+//         $('#todo-list').append('<p>' + task.content + '</p>');
+//       })
+//     },
+//     error: function (request, textStatus, errorMessage) {
+//       console.log(errorMessage);
+//     }
+//   });
+// });
